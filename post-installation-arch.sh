615d0e660e52611fdb10f503a4299dc6506f0f4b
nakouchdoge <nakouchdoge@gmail.com>
2024-05-13 14:33:02 -0500

    Readability improvements

--- post-installation-arch.sh
+++ post-installation-arch.sh
@@ -28,7 +28,7 @@ ask_yes_no () {
 #
 # Ask the user if they'd like to change the hostname of the machine.
 #
-if ask_yes_no "Change hostname?"; then
+if ask_yes_no ":: Change hostname?"; then
 	read -p "Enter new hostname: " new_hostname
 	if [ -f "/etc/hostname" ] && pacman -Q | grep -qF systemd; then
 		sudo hostnamectl hostname "$new_hostname" 
@@ -43,13 +43,13 @@ fi
 #
 # User gets added to the "docker" group to allow use of the docker commands without sudo.
 #
-if ask_yes_no "Update & upgrade system?"; then
+if ask_yes_no ":: Update & upgrade system?"; then
 	sudo pacman -Syu
 else
 	echo "Skipping."
 fi
 
-if ask_yes_no "Install docker?"; then
+if ask_yes_no ":: Install docker?"; then
 	echo "Installing docker"
 	sudo pacman -S docker docker-compose
 	echo "Docker installed."
@@ -61,11 +61,11 @@ fi
 #
 # User chosen or default basic packages.
 #
-if ask_yes_no "Install packages?"; then
-	if ask_yes_no "Install default packages? (base-devel, neovim, htop, neofetch, bat, ncdu, qemu-guest-agent, git, openssh)?"; then
+if ask_yes_no ":: Install packages?"; then
+	if ask_yes_no ":: Install default packages? (base-devel, neovim, htop, neofetch, bat, ncdu, qemu-guest-agent, git, openssh)?"; then
 		echo "Installing default pacakges."
 		sudo pacman -S base-devel neovim htop neofetch bat ncdu qemu-guest-agent git openssh
-		if ask_yes_no "Install more packages?"; then
+		if ask_yes_no ":: Install more packages?"; then
 			read -p "Type the packages you would like to install (separated by a single space): " packages_to_install
 			sudo pacman -S "$packages_to_install"
 		else
@@ -82,7 +82,7 @@ fi
 # Detect and ask if the user wants to enable these services in systemd
 #
 if pacman -Qe | grep -qF openssh; then
-	if ask_yes_no "OpenSSH installation detected, enable now?"; then
+	if ask_yes_no ":: OpenSSH installation detected, enable now?"; then
 		sudo systemctl enable --now sshd
 	else
 		echo "Skipping."
@@ -90,35 +90,71 @@ if pacman -Qe | grep -qF openssh; then
 fi
 
 if pacman -Qe | grep -qF docker; then
-	if ask_yes_no "Docker installation detected, enable now?"; then
+	if ask_yes_no ":: Docker installation detected, enable now?"; then
 		sudo systemctl enable --now docker.service
 	else
 		:
 	fi
 fi
 
-if pacman -Qe | grep -qF neovim; then
-	if ask_yes_no "NeoVIM installation detected, do you want 'v' 'vi' and 'vim' to be aliases for the 'nvim' command?"; then
-		if grep "alias vi='nvim'" /home/$USER/.bashrc && grep "alias vim='nvim'" /home/$USER/.bashrc && grep "alias v='nvim'" /home/$USER/.bashrc; then
-			echo "Aliases already exist. Skipping."
-		else
-			echo "alias vi='nvim'" >> /home/$USER/.bashrc
-			echo "alias vim='nvim'" >> /home/$USER/.bashrc
-			echo "alias v='nvim'" >> /home/$USER/.bashrc
-		fi
+if [ -f "/usr/bin/nvim" ]; then
+	if [ -f "/home/$USER/.bash_aliases" ] && grep -qF "source /home/$USER/.bash_aliases" "/home/$USER/.bashrc" && grep -qF "alias v='nvim'" "/home/$USER/.bash_aliases" && grep -qF "alias vi='nvim'" "/home/$USER/.bash_aliases" && grep -qF "alias vim='nvim'" "/home/$USER/.bash_aliases"; then
+		:
 	else
-		echo "Skipping."
+		if ask_yes_no ":: NeoVIM installation detected, do you want 'v', 'vi', and 'vim' commands to be aliased to the 'nvim' command?"; then
+			if [ -f "/home/$USER/.bash_aliases" ]; then
+				echo "alias v='nvim'" >> /home/$USER/.bash_aliases
+				echo "alias vi='nvim'" >> /home/$USER/.bash_aliases
+				echo "alias vim='nvim'" >> /home/$USER/.bash_aliases
+				echo "~/.bash_aliases file modified."
+				if [ -f "/home/$USER/.bashrc" ] && grep -qF "source /home/$USER/.bash_aliases" "/home/$USER/.bashrc"; then
+					echo "~/.bashrc points to ~/.bash_aliases already, ~/.bashrc has not been modified."
+				else
+					echo "source /home/$USER/.bash_aliases" >> /home/$USER/.bashrc
+					echo "~/.bashrc now points to ~/.bash_aliases, ~/.bashrc has been modified."
+				fi
+			else
+				touch /home/$USER/.bash_aliases
+				echo "alias v='nvim'" >> /home/$USER/.bash_aliases
+				echo "alias vi='nvim'" >> /home/$USER/.bash_aliases
+				echo "alias vim='nvim'" >> /home/$USER/.bash_aliases
+				echo "~/.bash_aliases file created and aliases added."
+				if [ -f "/home/$USER/.bashrc" ] && grep -qF "source /home/$USER/.bash_aliases" "/home/$USER/.bashrc"; then
+					echo "~/.bashrc points to ~/.bash_aliases already, ~/.bashrc has not been modified."
+				else
+					echo "source /home/$USER/.bash_aliases" >> /home/$USER/.bashrc
+					echo "~/.bashrc now points to ~/.bash_aliases, ~/.bashrc has been modified."
+				fi
+			fi
+		else
+			echo "Skipping."
+		fi
 	fi
 else
 	:
 fi
+#if pacman -Qe | grep -qF neovim; then
+#	if ask_yes_no ":: NeoVIM installation detected, do you want 'v' 'vi' and 'vim' to be aliases for the 'nvim' command?"; then
+#		if grep "alias vi='nvim'" /home/$USER/.bashrc && grep "alias vim='nvim'" /home/$USER/.bashrc && grep "alias v='nvim'" /home/$USER/.bashrc; then
+#			echo "Aliases already exist. Skipping."
+#		else
+#			echo "alias v='nvim'" >> /home/$USER/.bashrc
+#			echo "alias vi='nvim'" >> /home/$USER/.bashrc
+#			echo "alias vim='nvim'" >> /home/$USER/.bashrc
+#		fi
+#	else
+#		echo "Skipping."
+#	fi
+#else
+#	:
+#fi
 #
 # *TESTING*
 #
 # Arch bash prompt: PS1='\[\e[96m\]\u\[\e[96m\]@\[\e[96m\]\h\[\e[0m\]:\[\e[38;5;38m\]\w\[\e[0m\]\$ '
 # Ask to install the custom bash prompt. This is an "Arch blue" prompt.
 #
-if ask_yes_no "Add custom arch bash prompt?"; then
+if ask_yes_no ":: Add custom arch bash prompt?"; then
 #	if [ -f "/home/$USER/.bashrc" ] && grep -qF "PS1" "/home/$USER/.bashrc"; then
 	if [ -f "/home/$USER/.bashrc" ]; then
 		if [ -f "/home/$USER/.bash_prompt" ]; then
@@ -150,13 +186,13 @@ fi
 #
 # Ask the user if they'd like to add an automatic session timeout in a user defined number of seconds.
 #
-if ask_yes_no "Set a console timeout?"; then
+if ask_yes_no ":: Set a console timeout?"; then
 	read -p "Enter timeout in seconds: " tmout_seconds
 	if [ -f "/home/$USER/.bashrc" ]; then
 		if grep -qF "TMOUT=$tmout_seconds" "/home/$USER/.bashrc"; then
 			echo "TMOUT=$tmout_seconds already exists in ~/.bashrc. Skipping."
 		elif grep -qF "TMOUT=" "/home/$USER/.bashrc"; then
-			if ask_yes_no "A timeout already exists in ~/.bashrc. Overwrite?"; then
+			if ask_yes_no ":: A timeout already exists in ~/.bashrc. Overwrite?"; then
 				sed -i "s/TMOUT.*/TMOUT=$tmout_seconds/" ~/.bashrc
 				echo "A $tmout_seconds second timeout has been added and will be applied on next login or by running a new shell."
 			else
@@ -203,7 +239,7 @@ ssh_key_setup() {
 #
 # Backup any authorized_keys file that may exist and create a new one with user defined keys.
 # 
-if ask_yes_no "Modify authorized SSH keys? This will create a backup of ~/.ssh/authorized_keys and create a new one."; then
+if ask_yes_no ":: Modify authorized SSH keys? This will create a backup of ~/.ssh/authorized_keys and create a new one."; then
 	echo "Creating ~/.ssh/authorized_keys"
 	if [ -f "/home/$USER/.ssh/authorized_keys" ]; then
 		mv /home/$USER/.ssh/authorized_keys /home/$USER/.ssh/authorized_keys.old
@@ -213,7 +249,7 @@ if ask_yes_no "Modify authorized SSH keys? This will create a backup of ~/.ssh/a
 	fi
 	touch /home/$USER/.ssh/authorized_keys
 	sudo chown -R $USER /home/$USER/.ssh
-	ask_yes_no "Input SSH keys? (At least one is required for key authentication!) :"
+	ask_yes_no ":: Input SSH keys? (At least one is required for key authentication!) :"
 	ssh_key_setup
 else
 	echo "Skipping."
@@ -221,7 +257,7 @@ fi
 #
 # Ask user if they want to modify the default SSH port.
 #
-if ask_yes_no "Change SSH port?"; then
+if ask_yes_no ":: Change SSH port?"; then
 	read -p "Port to use for SSH: " ssh_port
 	echo "Changing default SSH port to $ssh_port."
 	if [ -f "/etc/ssh/sshd_config" ]; then
@@ -237,7 +273,7 @@ fi
 #
 # Add standard SSH security settings
 #
-if ask_yes_no "Change SSH security config?"; then
+if ask_yes_no ":: Change SSH security config?"; then
 	echo "Securing SSH for key-based authentication and removing root login."
 	if [ -f "/etc/ssh/sshd_config.d/10-security.conf" ]; then
 		sudo mv /etc/ssh/sshd_config.d/10-security.conf /etc/ssh/sshd_config.d/10-security.conf.old
@@ -267,7 +303,7 @@ else
 	echo "Skipping."
 fi
 
-if ask_yes_no "Restart sshd.service? If you changed the security config, you should answer yes."; then
+if ask_yes_no ":: Restart sshd.service? If you changed the security config, you should answer yes."; then
 	echo "Restarting sshd.service"
 	sudo systemctl restart sshd
 	sleep 3
@@ -295,7 +331,7 @@ firewallSetup() {
 # If the ufw package is installed, ask the user if they'd like to modify the firewall rules and enable it.
 #
 if ls /usr/sbin/ | grep "ufw"; then
-	if ask_yes_no "Modify firewall?"; then
+	if ask_yes_no ":: Modify firewall?"; then
 		firewallSetup
 	else
 		echo "Skipping."
@@ -305,7 +341,7 @@ else
 fi
 
 if ls /usr/sbin/ | grep "ufw"; then
-	if ask_yes_no "Enable firewall?"; then
+	if ask_yes_no ":: Enable firewall?"; then
 		sudo ufw enable
 	else
 		echo "Skipping."
@@ -314,7 +350,7 @@ else
 	echo "..."
 fi
 
-if ask_yes_no "Clone nvim configuration from git?"; then
+if ask_yes_no ":: Clone nvim configuration from git?"; then
 	git clone https://github.com/nakouchdoge/nvim /home/$USER/.config/nvim
 	echo "Git repository nakouchdoge/nvim has been cloned"
 else
--- post-installation-debian.sh
+++ post-installation-debian.sh
@@ -28,7 +28,7 @@ ask_yes_no () {
 #
 # Ask the user if they'd like to change the hostname of the machine.
 #
-if ask_yes_no "Change hostname?"; then
+if ask_yes_no ":: Change hostname?"; then
 	read -p "Enter new hostname: " new_hostname
 	if [ -f "/etc/hostname" ] && which systemd; then 
 		sudo hostnamectl hostname $new_hostname
@@ -42,13 +42,13 @@ fi
 # Ask the user if they'd like to install docker. First we update the system, get the keyring for docker, add the
 # repository, and install the packages through apt.
 #
-if ask_yes_no "Update & upgrade system?"; then
+if ask_yes_no ":: Update & upgrade system?"; then
 	sudo apt-get update && sudo apt-get upgrade -y
 else
 	echo "Skipping."
 fi
 
-if ask_yes_no "Install docker?"; then
+if ask_yes_no ":: Install docker?"; then
 	echo "Installing docker engine"
 
 	sudo apt-get install ca-certificates curl
@@ -77,11 +77,11 @@ fi
 #
 # User chosen or default basic packages.
 #
-if ask_yes_no "Install packages?"; then
-	if ask_yes_no "Install default packages? (neovim htop neofetch ncdu qemu-guest-agent git openssh-server)?"; then
+if ask_yes_no ":: Install packages?"; then
+	if ask_yes_no ":: Install default packages? (neovim htop neofetch ncdu qemu-guest-agent git openssh-server)?"; then
 		echo "Installing default pacakges."
 		sudo apt install neovim htop neofetch ncdu qemu-guest-agent git openssh-server
-		if ask_yes_no "Install more packages?"; then
+		if ask_yes_no ":: Install more packages?"; then
 			read -p "Type the packages you would like to install (separated by a single space): " packages_to_install
 			sudo apt install -S $packages_to_install
 		else
@@ -97,16 +97,16 @@ fi
 #
 # Detect and ask if the user wants to enable these services in systemd
 #
-if apt list --installed | grep -qF openssh; then
-	if ask_yes_no "OpenSSH installation detected, enable now?"; then
+if [ -f "/usr/sbin/sshd" ]; then 
+	if ask_yes_no ":: OpenSSH installation detected, enable now?"; then
 		sudo systemctl enable --now sshd
 	else
 		echo "Skipping."
 	fi
 fi
 
-if apt list --installed | grep -qF docker; then
-	if ask_yes_no "Docker installation detected, enable now?"; then
+if [ -f "/usr/bin/docker" ]; then 
+	if ask_yes_no ":: Docker installation detected, enable now?"; then
 		sudo systemctl enable --now docker.service
 	else
 		:
@@ -117,7 +117,7 @@ if [ -f "/usr/bin/nvim" ]; then
 	if [ -f "/home/$USER/.bash_aliases" ] && grep -qF "source /home/$USER/.bash_aliases" "/home/$USER/.bashrc" && grep -qF "alias v='nvim'" "/home/$USER/.bash_aliases" && grep -qF "alias vi='nvim'" "/home/$USER/.bash_aliases" && grep -qF "alias vim='nvim'" "/home/$USER/.bash_aliases"; then
 		:
 	else
-		if ask_yes_no "NeoVIM installation detected, do you want 'v', 'vi', and 'vim' commands to be aliased to the 'nvim' command?"; then
+		if ask_yes_no ":: NeoVIM installation detected, do you want 'v', 'vi', and 'vim' commands to be aliased to the 'nvim' command?"; then
 			if [ -f "/home/$USER/.bash_aliases" ]; then
 				echo "alias v='nvim'" >> /home/$USER/.bash_aliases
 				echo "alias vi='nvim'" >> /home/$USER/.bash_aliases
@@ -149,26 +149,10 @@ if [ -f "/usr/bin/nvim" ]; then
 else
 	:
 fi
-
-#if apt list --installed | grep -qF neovim; then
-#	if ask_yes_no "NeoVIM installation detected, do you want 'v' 'vi' and 'vim' to be aliases for the 'nvim' command?"; then
-#		if grep "alias vi='nvim'" /home/$USER/.bashrc && grep "alias vim='nvim'" /home/$USER/.bashrc && grep "alias v='nvim'" /home/$USER/.bashrc; then
-#			echo "Aliases already exist. Skipping."
-#		else
-#			echo "alias vi='nvim'" >> /home/$USER/.bashrc
-#			echo "alias vim='nvim'" >> /home/$USER/.bashrc
-#			echo "alias v='nvim'" >> /home/$USER/.bashrc
-#		fi
-#	else
-#		echo "Skipping."
-#	fi
-#else
-#	:
-#fi
 #
 # Ask to install the custom bash prompt. This is a "debian red" prompt.
 #
-if ask_yes_no "Add custom debian bash prompt?"; then
+if ask_yes_no ":: Add custom debian bash prompt?"; then
 	if [ -f "/home/$USER/.bashrc" ]; then
 		if [ -f "/home/$USER/.bash_prompt" ]; then
 			mv /home/$USER/.bash_prompt /home/$USER/.bash_prompt.bak
@@ -200,13 +184,13 @@ fi
 #
 # Ask the user if they'd like to add an automatic session timeout in a user defined number of seconds.
 #
-if ask_yes_no "Set a console timeout?"; then
+if ask_yes_no ":: Set a console timeout?"; then
 	read -p "Enter timeout in seconds: " tmout_seconds
 	if [ -f "/home/$USER/.bashrc" ]; then
 		if grep -qF "TMOUT=$tmout_seconds" "/home/$USER/.bashrc"; then
 			echo "TMOUT=$tmout_seconds already exists in ~/.bashrc. Skipping."
 		elif grep -qF "TMOUT=" "/home/$USER/.bashrc"; then
-			if ask_yes_no "A timeout already exists in ~/.bashrc. Overwrite?"; then
+			if ask_yes_no ":: A timeout already exists in ~/.bashrc. Overwrite?"; then
 				sed -i "s/TMOUT.*/TMOUT=$tmout_seconds/" ~/.bashrc
 				echo "A $tmout_seconds second timeout has been added and will be applied on next login or by running a new shell."
 			else
@@ -253,7 +237,7 @@ ssh_key_setup() {
 #
 # Backup any authorized_keys file that may exist and create a new one with user defined keys.
 # 
-if ask_yes_no "Modify authorized SSH keys? This will create a backup of ~/.ssh/authorized_keys and create a new one."; then
+if ask_yes_no ":: Modify authorized SSH keys? This will create a backup of ~/.ssh/authorized_keys and create a new one."; then
 	echo "Creating ~/.ssh/authorized_keys"
 	if [ -f "/home/$USER/.ssh/authorized_keys" ]; then
 		mv /home/$USER/.ssh/authorized_keys /home/$USER/.ssh/authorized_keys.old
@@ -263,7 +247,7 @@ if ask_yes_no "Modify authorized SSH keys? This will create a backup of ~/.ssh/a
 	fi
 	touch /home/$USER/.ssh/authorized_keys
 	sudo chown -R $USER /home/$USER/.ssh
-	ask_yes_no "Input SSH keys? (At least one is required for key authentication!) :"
+	ask_yes_no ":: Input SSH keys? (At least one is required for key authentication!) :"
 	ssh_key_setup
 else
 	echo "Skipping."
@@ -271,7 +255,7 @@ fi
 #
 # Ask user if they want to modify the default SSH port.
 #
-if ask_yes_no "Change SSH port?"; then
+if ask_yes_no ":: Change SSH port?"; then
 	read -p "Port to use for SSH: " ssh_port
 	echo "Changing default SSH port to $ssh_port."
 	if [ -f "/etc/ssh/sshd_config" ]; then
@@ -286,7 +270,7 @@ fi
 #
 # Add standard SSH security settings
 #
-if ask_yes_no "Change SSH security config?"; then
+if ask_yes_no ":: Change SSH security config?"; then
 	echo "Securing SSH for key-based authentication and removing root login."
 	if [ -f "/etc/ssh/sshd_config.d/10-security.conf" ]; then
 		sudo mv /etc/ssh/sshd_config.d/10-security.conf /etc/ssh/sshd_config.d/10-security.conf.old
@@ -316,7 +300,7 @@ else
 	echo "Skipping."
 fi
 
-if ask_yes_no "Restart sshd.service? If you changed the security config, you should answer yes."; then
+if ask_yes_no ":: Restart sshd.service? If you changed the security config, you should answer yes."; then
 	echo "Restarting sshd.service"
 	sudo systemctl restart sshd
 	sleep 3
@@ -344,7 +328,7 @@ firewallSetup() {
 # If the ufw package is installed, ask the user if they'd like to modify the firewall rules and enable it.
 #
 if ls /usr/sbin/ | grep "ufw"; then
-	if ask_yes_no "Modify firewall?"; then
+	if ask_yes_no ":: Modify firewall?"; then
 		firewallSetup
 	else
 		echo "Skipping."
@@ -354,7 +338,7 @@ else
 fi
 
 if ls /usr/sbin/ | grep "ufw"; then
-	if ask_yes_no "Enable firewall?"; then
+	if ask_yes_no ":: Enable firewall?"; then
 		sudo ufw enable
 	else
 		echo "Skipping."
@@ -363,7 +347,7 @@ else
 	echo "..."
 fi
 
-if ask_yes_no "Clone nvim configuration from git?"; then
+if ask_yes_no ":: Clone nvim configuration from git?"; then
 	git clone https://github.com/nakouchdoge/nvim /home/$USER/.config/nvim
 	echo "Git repository nakouchdoge/nvim has been cloned"
 else

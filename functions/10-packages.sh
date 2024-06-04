7d3f71ff2be6a99fd34c881a5fb3a66f87e7e302
nakouchdoge <nakouchdoge@gmail.com>
2024-06-04 14:47:00 -0500

    Removed c compilier check, added npm check

--- functions/10-packages.sh
+++ functions/10-packages.sh
@@ -223,16 +223,16 @@ function nvimConfig {
 function nvimEnsureConfig {
 	if grep -qF "https://github.com/nakouchdoge/nvim" "/home/$USER/.config/nvim/.git/config"; then
 		if ask_yes_no "${purple}:: Check if configuration is correct for nvim to work properly?${cr}"; then
-			if [ -f "/usr/bin/gcc" ] || [ -f "/usr/bin/cc" ] || [ -f "/usr/bin/clang" ] || [ -f "/usr/bin/cl" ] || [ -f "/usr/bin/zig" ]; then
-				echo "${green}Found C Compiler${cr}"
+			if [ -f "/usr/bin/npm" ]; then 
+				echo "${green}Found NPM${cr}"
 			else
-				if ask_yes_no "${red}:: No C compiler found, install GCC?${cr}"; then
+				if ask_yes_no "${red}:: NPM executable not found, install now?${cr}"; then
 					sudo apt install gcc
 				else
 					skipping
 				fi
 			fi
-			if nvim --version | grep -qF 0.9.; then
+			if nvim --version | grep -qF 0.9. || nvim --version | grep -qF 0.10.; then
 				echo "${green}Found NeoVIM Version 0.9+${cr}"
 			else
 				echo "${red}You might be running an older version of neovim, if you run into issues, consider updating.${cr}"
